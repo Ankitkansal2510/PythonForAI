e00f7f59489b3d34dddafd1f628b56bc99d2cca7
ankitkansal <kansal920@gmail.com>
2025-12-18 00:12:33 +0530

    tuples example

---
+++ tuples.py
@@ -0,0 +1,11 @@
+##Tuples are immutable , once created cannot be modified
+
+a=(1,2,3)
+print('printing tuple',a)
+print(type(a))
+
+
+b=(1) #here python understand it is a integer so in order to make it understand that this is the tuple
+c=(1,)
+print("type of be is : " , type(b))
+print("Type of c is :" , type(c))
